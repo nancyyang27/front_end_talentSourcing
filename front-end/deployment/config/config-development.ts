6eb45689cad946e4e4cc51d6c3151ae153c40927
Nancy Yang <nancyyang@Nancys-MacBook-Pro.local>
2023-01-09 18:23:39 -0800

    updated setup

--- front-end/deployment/config/config-development.ts
+++ front-end/deployment/config/config-development.ts
@@ -1,7 +1,7 @@
 /**
  * Add your hosted dev URL here
  */
-const HOSTED_URL = 'https://dev.domain.com';
+const HOSTED_URL = 'https://nancy.d3317vxvbi870t.amplifyapp.com';
 
 const configDevelopment = {
   HOSTED_URL,
@@ -14,10 +14,10 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '',
-  USER_POOL_ID: '',
-  IDENTITY_POOL_ID: '',
-  GRAPHQL_ENDPOINT: '',
+  USER_POOL_CLIENT_ID: '1m2k9p97so9o1himfsmtjm3kdd',
+  USER_POOL_ID: 'us-east-1_Xkeq4OghR',
+  IDENTITY_POOL_ID: 'us-east-1:a76311f9d1d5-473f-8ef5-1af2d0c9b949',
+  GRAPHQL_ENDPOINT: 'https://urvhige3ljgdjcnndfepxk6vwa.appsync-api.us-east-1.amazonaws.com/graphql',
 };
 
 export default configDevelopment;
