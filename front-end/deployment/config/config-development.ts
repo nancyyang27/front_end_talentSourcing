c9a49cd2d9ddb84a471348fff3658b3e29f16112
Nancy Yang <nancyyang@Nancys-MacBook-Pro.local>
2023-01-17 20:18:35 -0800

    update file

--- front-end/deployment/config/config-development.ts
+++ front-end/deployment/config/config-development.ts
@@ -6,7 +6,7 @@ const HOSTED_URL = 'https://nancy.d3317vxvbi870t.amplifyapp.com';
 const configDevelopment = {
   HOSTED_URL,
   MODE: 'DEVELOPMENT',
-  REGION: 'eu-west-1',
+  REGION: 'us-east-1',
   REDIRECT_SIGN_IN: `${HOSTED_URL}/`,
   REDIRECT_SIGN_OUT: `${HOSTED_URL}/signout/`,
   AUTHENTICATION_TYPE: 'AWS_IAM' as const,
@@ -14,10 +14,10 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '1m2k9p97so9o1himfsmtjm3kdd',
-  USER_POOL_ID: 'us-east-1_Xkeq4OghR',
-  IDENTITY_POOL_ID: 'us-east-1:a76311f9d1d5-473f-8ef5-1af2d0c9b949',
-  GRAPHQL_ENDPOINT: 'https://urvhige3ljgdjcnndfepxk6vwa.appsync-api.us-east-1.amazonaws.com/graphql',
+  USER_POOL_CLIENT_ID: '18oa3e1fhqnlv3ooqe9ffo8i2a',
+  USER_POOL_ID: 'us-east-1_xxIKf4fFr',
+  IDENTITY_POOL_ID: 'us-east-1:ca4626d3-9143-45ef-97f0-c21e9d0d3b99',
+  GRAPHQL_ENDPOINT: 'https://6tv5k4ptubclxeu7nbpomsd3d4.appsync-api.us-east-1.amazonaws.com/graphql',
 };
 
 export default configDevelopment;
