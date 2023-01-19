d1f2ad963436393322431c0d6bc7864ba2001c80
nancyyang27 <yyang72@uw.edu>
2023-01-18 18:28:16 -0800

    push changes

--- front-end/graphql/API.ts
+++ front-end/graphql/API.ts
@@ -1,3 +1,16 @@
 /* tslint:disable */
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
+export type CognitoUser = {
+  __typename: "CognitoUser",
+  id: string,
+  username: string,
+};
+
+export type GetCognitoUserQuery = {
+  getCognitoUser?:  {
+    __typename: "CognitoUser",
+    id: string,
+    username: string,
+  } | null,
+};
